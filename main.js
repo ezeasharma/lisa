2835b76bdd776eb7c7f5169d75e9766925dfe47a
Ashish <emailashish1@gmail.com>
2015-10-22 10:38:25 -0400

    port as env variable

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ app.get('/', function(req, res){
 	res.send('Hello! This is Lisa, your personel assistant for baby activity tracking!');
 });
 
-var server = app.listen(5000, function(){
+var server = app.listen(process.env.PORT || 5000, function(){
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Listening at http://%s:%s', host, port);
